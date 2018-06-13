81cf554bd80fc75b7b854a19fba49c6166895f1d
JBrow95 <browningjustin95@gmail.com>
2018-06-13 15:22:09 -0400

    Created multiple funcs with .sample arrays

---
+++ pizza_app.rb
@@ -0,0 +1,37 @@
+	
+def rpizza()
+
+	puts "Here is a random pizza!"
+	puts "#{size}#{crust}#{meats}#{meats2}#{veggies}#{fruits}"
+	
+end
+
+def size()
+	size = ["small", "medium", "large"]
+	puts "Size: #{size.sample}"
+end
+
+def crust()
+	crust = ['thin', 'pan']
+	puts "Crust: #{crust.sample}"
+end
+
+def meats()
+	m1 = ['none', 'bacon', 'ham', 'pepperoni', 'sausage']
+	puts "Meat Toppings: #{m1.sample}"
+end
+
+def meats2()
+	m2 = ['none', 'bacon', 'ham', 'pepperoni', 'sausage']
+	puts "2nd Meat Toppings: #{m2.sample}"
+end
+
+def veggies()
+	vegs = ['tomatoe', 'onion', 'peppers', "none"]
+	puts "Vegetables Toppings: #{vegs.sample}"
+end
+
+def fruits()
+	f = ["pineapple", "none"]
+	puts "Fruit Toppigss: #{f.sample}"
+end
